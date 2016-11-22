73ab6bba54a2e82da9206269dc239dab1b532935
Vitalii Chechel <vitalii_chechel@epam.com>
2016-11-23 00:46:55 +0200

    added secyrity setting

--- src/main/java/com/chechel/spring/config/AppConfig.java
+++ src/main/java/com/chechel/spring/config/AppConfig.java
@@ -3,10 +3,9 @@ package com.chechel.spring.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan(basePackages = {"com.chechel.spring"})
-@Import(value = {WebMvcConfig.class})
+@Import(value = {WebMvcConfig.class, WebSecurityConfig.class})
 public class AppConfig {
 }
--- src/main/java/com/chechel/spring/config/WebSecurityConfig.java
+++ src/main/java/com/chechel/spring/config/WebSecurityConfig.java
@@ -1,8 +1,36 @@
 package com.chechel.spring.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-//@EnableWebSecurity
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.
+                authorizeRequests()
+                .antMatchers("/hello")
+                .hasRole("ADMIN")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll();
+    }
+//
+//    @Bean
+//    public UserDetailsService userDetailsService() throws Exception {
+//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//        manager.createUser(User.withUsername("user").password("password").roles("USER").build());
+//        manager.createUser(User.withUsername("admin").password("password").roles("USER","ADMIN").build());
+//        return manager;
+//    }
 }
